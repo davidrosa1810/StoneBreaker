eddc09cedfad1b5592ab674f980792ee0ac277d9
davidrosa1810 <dmr.rosa@gmail.com>
2022-01-09 13:00:53 +0000

    Added Dependencies
    Included gui,oberver and utils packages

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import utils.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Point2D p = new Point2D(2,2);
+        System.out.println(p);
     }
 }
